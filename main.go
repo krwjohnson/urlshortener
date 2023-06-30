13455b09810798c2f1487eae5bf9cf8d1cbb6795
Kristen Johnson <krwjohnson@icloud.com>
2023-06-30 15:42:18 +1000

    Made the URLs shorter

--- main.go
+++ main.go
@@ -5,18 +5,20 @@ import (
 	"time"
 	"net/http"
 	"strings"
+	"math/big"
 	"github.com/gorilla/mux"
 	"context"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"crypto/rand"
-	"encoding/hex"
 	"html/template"
 )
 
 func main() {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	collection := client.Database("test").Collection("urls")
 
@@ -37,32 +39,41 @@ func HomeHandler(collection *mongo.Collection) func(w http.ResponseWriter, r *ht
 }
 
 
-func connectDB() (*mongo.Collection, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+// func connectDB() (*mongo.Collection, error) {
+// 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		return nil, err
-	}
+// 	client, err := mongo.Connect(context.TODO(), clientOptions)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		return nil, err
-	}
+// 	err = client.Ping(context.TODO(), nil)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	collection := client.Database("urlshortener").Collection("urls")
+// 	collection := client.Database("urlshortener").Collection("urls")
 
-	return collection, nil
-}
+// 	return collection, nil
+// }
 
 func generateID() (string, error) {
-	b := make([]byte, 4) // Change this to increase/decrease ID length
-	_, err := rand.Read(b)
-	if err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(b), nil
+    const charset = "abcdefghijklmnopqrstuvwxyz" + 
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                    "0123456789"
+    const idLength = 4
+
+    b := make([]byte, idLength)
+    for i := range b {
+        num, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+        if err != nil {
+            return "", err
+        }
+        b[i] = charset[num.Int64()]
+    }
+    return string(b), nil
 }
+
 
 func RedirectHandler(collection *mongo.Collection) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -85,39 +96,58 @@ func RedirectHandler(collection *mongo.Collection) func(w http.ResponseWriter, r
 }
 
 func CreateHandler(collection *mongo.Collection) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()
-		url := r.Form.Get("url")
+    return func(w http.ResponseWriter, r *http.Request) {
+        tmpl := template.Must(template.ParseFiles("templates/index.html"))
+        r.ParseForm()
+        url := r.Form.Get("url")
 
-		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
-			url = "http://" + url
-		}
+        if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+            url = "http://" + url
+        }
 
-		id, err := generateID()
-		if err != nil {
-			http.Error(w, "Error generating ID", http.StatusInternalServerError)
-			return
-		}
+        var id string
+        for {
+            var err error
+            id, err = generateID()
+            if err != nil {
+                http.Error(w, "Error generating ID", http.StatusInternalServerError)
+                return
+            }
 
-		newURL := &URL{
-			ID:        id,
-			CreatedAt: time.Now(),
-			Dest:      url,
-		}
+            var result URL
+            err = collection.FindOne(context.Background(), bson.M{"id": id}).Decode(&result)
 
-		_, err = collection.InsertOne(context.Background(), newURL)
-		if err != nil {
-			http.Error(w, "Error inserting URL into database", http.StatusInternalServerError)
-			return
-		}
+            if err == mongo.ErrNoDocuments {
+                // This ID does not exist in the database, so we can use it.
+                break
+            }
 
-		data := struct {
-			ShortURL string
-		}{
-			ShortURL: "http://localhost:8080/" + id,
-		}
+            if err != nil {
+                http.Error(w, "Error searching in database", http.StatusInternalServerError)
+                return
+            }
+            // This ID exists in the database, so we continue the loop to generate a new one.
+        }
 
-		tmpl := template.Must(template.ParseFiles("templates/index.html"))
-		tmpl.Execute(w, data)
-	}
+        newURL := &URL{
+            ID:        id,
+            CreatedAt: time.Now(),
+            Dest:      url,
+        }
+
+        _, err := collection.InsertOne(context.Background(), newURL)
+        if err != nil {
+            http.Error(w, "Error inserting URL into database", http.StatusInternalServerError)
+            return
+        }
+
+        data := struct {
+            ShortURL string
+        }{
+            ShortURL: "http://localhost:8080/" + id,
+        }
+
+        tmpl.Execute(w, data)
+    }
 }
+
